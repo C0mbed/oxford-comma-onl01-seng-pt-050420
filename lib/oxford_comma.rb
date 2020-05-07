c69e7032980fbac39edaf62d6334551ae440695b
Matt Farncombe <matt@farncombe.co>
2020-05-07 01:16:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array[0..-2].join(", ") + " and " + array[-1]
+  if array.length = 1 
+    return array[0]
+  elsif array.length = 2 
+else
+   array[0..-2].join(", ") + " and " + array[-1]
 end
